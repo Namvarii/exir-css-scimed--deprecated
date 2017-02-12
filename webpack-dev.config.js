ac40978d02e71d31d33c4ee958e9e342dfed59d8
Navid Farahzadi <na.farahzadi@gmail.com>
2017-02-12 16:56:14 +0330

    vazir font added header created

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -5,46 +5,56 @@ const webpack = require( 'webpack' )
 const path = require( 'path' )
 
 module.exports = function() {
-	//noinspection JSUnresolvedFunction
-	return {
-		entry: {
-			app: path.resolve( __dirname, 'src', 'js', 'app.js' ),
-			vendor: path.resolve( __dirname, 'src', 'js', 'vendor.js' )
-		},
-		output: {
-			filename: '[name].bundle.js',
-			path: path.resolve( __dirname, 'dist' )
-		},
-		module: {
-			rules: [
-				{
-					test: /\.scss/,
-					exclude: /node_modules/,
-					use: [
-						{
-							loader: 'style-loader'
-						},
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				}
-			]
-		},
-		plugins: [
-			new webpack.optimize.CommonsChunkPlugin(
-					{ names: [ 'vendor' ] }
-			)
-		],
-		devtool: 'source-map'
-	}
+   return {
+      entry: {
+         app: path.resolve( __dirname, 'src', 'js', 'app.js' ),
+         vendor: path.resolve( __dirname, 'src', 'js', 'vendor.js' )
+      },
+      output: {
+         filename: '[name].bundle.js',
+         path: path.resolve( __dirname, 'dist' )
+      },
+      module: {
+         rules: [
+            {
+               test: /\.(woff|woff2|ttf|eot)$/,
+               use: {
+                  loader: 'url-loader',
+                  options: {
+                     limit: '10000'
+                  }
+               }
+            },
+            {
+               test: /\.js(x)?$/,
+               exclude: /node_modules/,
+               use: 'babel-loader'
+            },
+            {
+               test: /\.scss$/,
+               exclude: /node_modules/,
+               use: [
+                  {
+                     loader: 'style-loader'
+                  },
+                  {
+                     loader: 'css-loader',
+                     options: {
+                        sourceMap: true
+                     }
+                  },
+                  {
+                     loader: 'sass-loader',
+                     options: {
+                        sourceMap: true
+                     }
+                  }
+               ]
+            }
+         ]
+      },
+      devtool: 'source-map',
+      stats: 'verbose',
+      watch: true
+   }
 }
